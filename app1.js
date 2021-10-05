5acf244c22d1788216f6a705a2800fc34c443c60
srcnllc <56175274+srcnllc@users.noreply.github.com>
2021-10-05 11:26:40 +0300

    portfolyo

---
+++ app1.js
@@ -0,0 +1,50 @@
+function sayfayagit(){
+    location.href="anasayfa1.html";
+}
+function basla(){
+    var zaman = new Date();
+    var yıl = zaman.getFullYear();
+    var dogumyılı = 1993;
+    var yas = document.getElementById("yas").innerHTML =" " +Number(yıl-dogumyılı);
+}
+function indirbir(){
+    location.href="https://drive.google.com/file/d/1s5B5Nma3rmBC0FHZAh4OSe8JVALs3ClW/view?usp=sharing";
+}
+function indiriki(){
+    location.href="https://drive.google.com/file/d/17V2dsdN32D5QlIciToNCYHLiLgxC6o3V/view?usp=sharing";
+}
+function anasayfa(){
+    location.href="index.html";
+
+}
+function hakkımda(){
+    location.href="#hakkında";
+
+}
+function egitim(){
+    location.href="#egitim";
+
+}
+function sertifika(){
+    location.href="#sertifikalar";
+
+}
+function proje(){
+    location.href="#projeler";
+
+}
+function iletisim(){
+    location.href="#iletisim";
+
+}
+function izle1(){
+    location.href="https://drive.google.com/file/d/1y9sN_mxyLUQPU7g0HpyZa15IE2a6WPq0/view?usp=sharing";
+}
+function izle2(){
+    location.href="https://drive.google.com/file/d/1kRQjW-ldqgNuxHTsEZLYocgd79VMsgio/view?usp=sharing";
+
+}
+function izle3(){
+    location.href="https://drive.google.com/file/d/16i0CF8tKKRil1PoOObh4wGFtIuorTfZm/view?usp=sharing";
+
+}
